a779343ac6fc040f3daa80a73396c8edcc18ea8f
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-09-18 14:33:21 +0100

    updated courses

--- Lesson-15 (Asynchronus in details)/app.js
+++ Lesson-15 (Asynchronus in details)/app.js
@@ -1,17 +1,5 @@
 console.log("Hello World");
 
-// const functionOne = () => {
-//    console.log("Function One");
-//    functionTwo();
-//    console.log("Function One, Part Two");
-// };
-
-// const functionTwo = () => {
-//    console.log("Function Two");
-// }
-
-// functionOne();
-
 const functionOne = () => {
   console.log("Function One");
   functionTwo();
@@ -19,67 +7,148 @@ const functionOne = () => {
 };
 
 const functionTwo = () => {
-  setTimeout(() => {
-    console.log("Function Two");
-  }, 3000);
+  console.log("Function Two");
 };
 
 functionOne();
 
-// Simulating data fetching
-
+//********************************************************
 // const fetchUser = (username) => {
-//     setTimeout(() => {
-//         return {user: username }
-//     }, 2000)
-// };
-// const user = fetchUser("Bolaji");
+//    setTimeout(() => {
+//       return{user: username};
+//    }, 3000)
+// }
+
+// const user = fetchUser('Bolaji');
 // console.log(user);
 
-const fetchUser = (username, callback) => {
-  setTimeout(() => {
-    return callback({ user: username });
-  }, 2000);
-};
+// const fetchUser = (username, callback) => {
+//    setTimeout(() => {
+//       return callback({user: username});
+//    }, 3000)
+// }
 
-const fetchUserPhoto = (callback) => {
-  setTimeout(() => {
-    return callback(["Photo1", "Photo2"]);
-  }, 2000);
-};
+// const fetchUserPhoto = (callback) => {
+//    setTimeout(() => {
+//       return callback(['Photo1', 'Photo2'])
+//    }, 2000)
+// }
 
-const fetchUserMail = (callback) => {
-  setTimeout(() => {
-    return callback({ email: "lagbaja@gmail.com" });
-  }, 2000);
-};
+// const fetchUserMail = (callback) => {
+//    setTimeout(() => {
+//       return callback({email: 'lagbaja@gmail.com'})
+//    }, 2000)
+// }
 
-const fetchUserFriends = (callback) => {
-  setTimeout(() => {
-    return callback({ friends: "Muhammed", friends2: "Rocco" });
-  }, 2000);
-};
+// const fetchUserFriends  = (callback) => {
+//    setTimeout(() => {
+//       return callback({friend1:'Bolaji', friend2: 'Kemi', friend3: 'Kunle'})
+//    }, 2000)
+// }
 
-fetchUser("Bolaji", (user) => {
-  console.log(user);
-  fetchUserPhoto((userPhoto) => {
-    console.log(userPhoto);
-    fetchUserMail((email) => {
-      console.log(email);
-    });
-    fetchUserFriends((userFriends) => {
-      console.log(userFriends);
-    });
-  });
+// fetchUser('Bolaji', (user) => {
+//    console.log(user);
+//    fetchUserPhoto((userPhoto) => {
+//       console.log(userPhoto);
+//       fetchUserMail((email) => {
+//          console.log(email);
+//          fetchUserFriends((userFriends) => {
+//             console.log(userFriends);
+//          })
+//       })
+//    })
+// });
+
+// ************************************************PROMISE**************************************
+// SYNTAX
+
+// new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//       reject();
+//    }, 2000)
+// }).then(() => {  //.then is needed
+// }).catch(() => {
+// })
+
+// new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//       // resolve({user: "Adrian"})
+//       reject('Error fetching the data');
+//    }, 2000)
+// }).then((user) => {  //.then is needed
+//    console.log(user);
+// }).catch((err) => {
+//    console.log(err);
+// })
+
+// *******************************************************
+const promise = new Promise((resolve, reject) => {
+  resolve();
+  reject();
 });
-// console.log(user);
 
-// using Of promise
+promise.then(() => {}).catch(() => {});
 
-new Promise ((resolve,reject)=>{
-  setTimeout(()=>{
-    resolve()
-  },2000)
-}).then((user)=>{
-console.log(user);
-})
+// *******************************************************
+const fetchUser = (username) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ user: username });
+      reject("Error fetching user");
+    }, 2000);
+  });
+};
+
+const fetchUserPhoto = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("User photo fetched successfully");
+      resolve(["Photo1", "Photo2"]);
+      reject("Photo not fetched");
+    }, 2000);
+  });
+};
+
+const fetchUserMail = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("mail fetched successfully");
+      resolve({ email: "lagbaja@gmail.com" });
+      reject("email not fetched");
+    }, 2000);
+  });
+};
+
+// ********Promises fetch using .then and catch******
+// fetchUser("Ali")
+//   .then((user) => console.log(user))
+//   .then((user) => fetchUserPhoto(user))
+//   .then((userPhoto) => console.log(userPhoto))
+//   .then((user) => fetchUserMail(user))
+//   .then((userMail) => console.log(userMail))
+//   .catch((err) => console.log(err));
+
+// ***********Async/await promises Method*********
+const fetchNumber = async () => {
+  return 25;
+};
+// console.log(fetchNumber().then((number) => console.log(number)));
+
+const consoleFetchNumber = async () => {
+  const fetch = await fetchNumber();
+  console.log(fetch);
+};
+
+consoleFetchNumber();
+
+const displayData = async () => {
+  const user = await fetchUser("MR. Supreme");
+  const userPhoto = await fetchUserPhoto();
+  const userMail = await fetchUserMail();
+
+  console.log(user);
+  console.log(userPhoto);
+  console.log(userMail);
+};
+
+displayData();
---
+++ Lesson-16 (Higher Order Function)/app.js
@@ -0,0 +1,146 @@
+//Higher Order Function
+//* For Each Method
+//* For Filter Method
+//* For Map Method
+//* For Sort Method
+//* For Reduce Method
+
+// Why function Takes Pram?
+function double(n) {
+  return n * 2;
+}
+console.log(double(5));
+
+const companies = [
+  { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+  { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+  { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+  { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+  { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+  { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//for loop method
+for (let i = 0; i < companies.length; i++) {
+  console.log(companies[i]);
+}
+
+//for Each Method
+companies.forEach(function (Company) {
+  console.log(Company);
+  console.log(Company.name);
+});
+
+// let canDrink = []
+// for (let i = 0; i< ages.length; i++) {
+//     if(ages [i] >= 21){
+//         canDrink.push(ages[i])
+
+//     }
+
+// }
+// console.log(canDrink);
+
+// Filter
+// const canDrink = ages.filter(function(age){
+//     if(age>=21){
+//         return true
+//     }
+// })
+// console.log(canDrink);
+
+const canDrink = ages.filter((age) => age >= 21);
+console.log(canDrink);
+
+const retailCompany = companies.filter(function (Company) {
+  if (Company.category == "Retail") {
+    return true;
+  }
+});
+console.log(retailCompany);
+
+const retailCompanies = companies.filter(
+  (Company) => Company.category === "Retail"
+);
+console.log(retailCompanies);
+
+const lasted10Years = companies.filter(
+  (Company) => Company.end - Company.start >= 10
+);
+console.log(lasted10Years);
+
+// map method
+
+const companies1 = companies.map(function (Company) {
+  return Company.name;
+});
+console.log(companies1);
+// map through compant name,start and end using function decleration
+const companies2 = companies.map(function (Company) {
+  console.log(Company.name, Company.start, Company.end);
+});
+
+const companies3 = companies.map(
+  (Company) => `${Company.name} [${Company.start} - ${Company.end}]`
+);
+
+console.log(companies3);
+
+// multiplying the root of ages
+
+const ageMap = ages.map((age) => Math.sqrt(age)).map((age) => age * 2);
+console.log(ageMap);
+
+// Sort Mthod
+
+// const sortedCompanies = companies.sort(function(c1,c2) {
+//     if(c1.start > c2.start) {
+//         return 1
+//     }else{
+//         return -1
+//     }
+// })
+// console.log(sortedCompanies);
+
+const sort = companies.sort((c1, c2) => c1.start > c2.start);
+console.log(sort);
+
+const age1 = ages.sort((a,b) => a-b)
+console.log(age1);
+
+//************ reduce method
+// for loop method
+let ageSum = 0
+for (let i = 0; i < ages.length; i++) {
+   ageSum += ages[i]
+}
+console.log(ageSum);
+
+
+// reduce method
+const ageSum1 = ages.reduce(function(total,age){
+    return total+age
+}, 0)
+console.log(ageSum1);
+
+//Using Arrow Function
+
+const ageSum2 = ages.reduce((total, age) => total+age, 0)
+console.log(ageSum2);
+
+const totalYears = companies.reduce((total,Company) => total+ (Company.end - Company.start),0)
+console.log(totalYears);
+
+// combination of all methods
+
+const combined = ages
+.map(age => age*2)
+.filter(age => age >= 40)
+.sort((a,b) => a-b)
+.reduce((a,b) => a+b, 0)
+console.log(combined);
